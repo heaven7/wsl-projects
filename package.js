0c58d1577e615067448d6ae6fb4c982537c57f6c
Christian Grlica <christian@heavenseven.net>
2015-12-29 14:28:33 +0100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'heaven7:wsl-projects',
-  version: '0.0.3_1',
+  version: '0.0.3_2',
   summary: 'Projects package',
   git: 'https://github.com/heaven7/wsl-projects.git',
   documentation: 'README.md'
